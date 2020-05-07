134a6f6c9efea306a365228b2a43d6ab38fe6446
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-05-07 21:22:36 +0200

    Observing Different Types of Responses

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
-import {map, catchError} from "rxjs/operators";
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpEventType, HttpHeaders, HttpParams} from "@angular/common/http";
+import {catchError, map, tap} from "rxjs/operators";
 import {Post} from "./post.model";
 import {Subject, throwError} from "rxjs";
 
@@ -17,7 +17,10 @@ export class PostsService {
     this.http
       .post<{ name: string }>(
         'https://ngcg-http.firebaseio.com/posts.json',
-        postData
+        postData,
+        {
+          observe: 'response'
+        }
       )
       .subscribe(responseData => {
         console.log(responseData);
@@ -56,6 +59,19 @@ export class PostsService {
   }
 
   deletePosts() {
-    return this.http.delete('https://ngcg-http.firebaseio.com/posts.json');
+    return this.http.delete(
+      'https://ngcg-http.firebaseio.com/posts.json',
+      {
+        observe: 'events'
+      }
+    ).pipe(tap(event => {
+      console.log(event);
+      if (event.type === HttpEventType.Sent) {
+        // ...
+      }
+      if (event.type === HttpEventType.Response) {
+        console.log(event.body);
+      }
+    }));
   }
 }
