eaf0e47373c0a8431ca19096463a378b9f7b5b42
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-05-07 21:48:31 +0200

    Response Interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,4 +1,5 @@
-import {HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {tap} from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor{
 
@@ -6,7 +7,13 @@ export class AuthInterceptorService implements HttpInterceptor{
     console.log('Request is on its way');
     console.log(req.url);
     const modifiedRequest = req.clone({headers: req.headers.append('Auth','xyz')});
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest).pipe(tap(event => {
+      console.log(event);
+      if (event.type === HttpEventType.Response) {
+        console.log('Response arrived, body data: ');
+        console.log(event.body);
+      }
+    }));
   }
 
 }
